fc72fd1b022e3b742dab1f8b7d62d58fbb07829e
hmsgobrr <metalcorehms@gmail.com>
2022-12-07 13:52:32 +0700

    moved GLErrorManager functions into Renderer.cpp

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -3,14 +3,91 @@
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
 
+#include <cstdlib> // for exit()
+
 #include "Log.h"
-#include "GLErrorManager.h"
 
 #ifndef ASSETS_PATH
 #define ASSETS_PATH "../assets/"
 #endif
 
 namespace pt {
+
+static void GLAPIENTRY GLLogMessage(GLenum source, GLenum type, GLuint id, GLenum severity,
+    GLsizei length, const GLchar* message, const void* userParam) {
+    char* _source;
+    char* _type;
+
+    switch (source) {
+    case GL_DEBUG_SOURCE_API:
+        _source = (char*)"API";
+        break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = (char*)"WINDOW SYSTEM";
+        break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = (char*)"SHADER COMPILER";
+        break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = (char*)"THIRD PARTY";
+        break;
+    case GL_DEBUG_SOURCE_APPLICATION:
+        _source = (char*)"APPLICATION";
+        break;
+    case GL_DEBUG_SOURCE_OTHER:
+        _source = (char*)"UNKNOWN";
+        break;
+    default:
+        _source = (char*)"UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+    case GL_DEBUG_TYPE_ERROR:
+        _type = (char*)"ERROR";
+        break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = (char*)"DEPRECATED BEHAVIOR";
+        break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = (char*)"UDEFINED BEHAVIOR";
+        break;
+    case GL_DEBUG_TYPE_PORTABILITY:
+        _type = (char*)"PORTABILITY";
+        break;
+    case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = (char*)"PERFORMANCE";
+        break;
+    case GL_DEBUG_TYPE_OTHER:
+        _type = (char*)"OTHER";
+        break;
+    case GL_DEBUG_TYPE_MARKER:
+        _type = (char*)"MARKER";
+        break;
+    default:
+        _type = (char*)"UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+    case GL_DEBUG_SEVERITY_HIGH:
+        PT_CORE_CRITICAL("OpengGL Critical Error ({}): {} from {}. {}", id, _type, _source, message);
+        exit(-1);
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        PT_CORE_ERROR("OpengGL Error ({}): {} from {}. {}", id, _type, _source, message);
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        PT_CORE_WARN("OpengGL Minor Error ({}): {} from {}. {}", id, _type, _source, message);
+        break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        PT_CORE_INFO("OpengGL Info ({}): {} from {}. {}", id, _type, _source, message);
+        break;
+    default:
+        PT_CORE_INFO("OpengGL Unknown Notification ({}): {} from {}. {}", id, _type, _source, message);
+        break;
+    }
+}
 
 void Renderer::init(GLFWwindow* contextWindow, float p_frameWidth, float p_frameHeight) {
     glfwMakeContextCurrent(contextWindow);
